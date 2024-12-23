a61f16457c5f803e58e1edf2ed52e061b240c512
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-12-23 15:04:27 -0500

    cambio API url

--- src/componets/Core.js
+++ src/componets/Core.js
@@ -20,7 +20,7 @@ const Core = () => {
 
         setLoading(true);
         try {
-            const response = await fetch("http://127.0.0.1:5000/gastos/filtrar", {
+            const response = await fetch("https://api-web-latest-nbk1.onrender.com//gastos/filtrar", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
